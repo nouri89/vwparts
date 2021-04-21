dd65eadff0039e208b7ff98eff4b7e0a5001bf99
nouri89 <nouri89@hotmail.com>
2021-04-21 03:54:24 +0100

    refine pool

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,3 @@
 const { Pool } = require('pg')
 const pool = new Pool()
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  },
-}
+module.exports = pool;
